297eed5e9fab0b2f226ecdc691e9cd0dc3ca26f7
Николай Серваков <41160218+servakov@users.noreply.github.com>
2020-06-25 15:37:59 +0300

    add plugin url

--- info.php
+++ info.php
@@ -5,7 +5,7 @@ $info = array(
 	'description' => t('Пагинация страниц (&lt;&lt; &lt; 1 2 ... 5 6 7 &gt; &gt;&gt;)<br>Нажимая <b>PgDn</b>|<b>&rarr;</b>|<b>Пробел</b> в конце страницы, переход на следующую страницу<br>Нажимая <b>PgUp</b>|<b>&larr;</b>|<b>Backspace</b> в начале страницы, переход на предыдущую страницу <br>По мотивам <a href="https://habr.com/ru/sandbox/77604/" target="_blank">https://habr.com/ru/sandbox/77604/</a>'),
 	'version' => '2.2',
 	'author' => 'servakov',
-	'plugin_url' => '',
+	'plugin_url' => '//github.com/servakov/pagination_pgdn',
 	'author_url' => '//wpgrabber-tune.blogspot.com/',
 	'group' => 'template'
 );
